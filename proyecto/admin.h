26f6104fca429796da8fe4934850605f792904d7
Irene García Aranzabal <irene.g.aranzabal@opendeusto.es>
2026-04-10 08:31:17 +0200

    nuevos archivos admin.*

---
+++ proyecto/admin.h
@@ -0,0 +1,13 @@
+#ifndef ADMIN_H_
+#define ADMIN_H_
+#include "sqlite3.h"
+#include "config.h"
+
+/*muestra el login en pantalla y pide usuario y contraseña al usuario.*/
+int admin_login(const Config *cfg); /*este const no permite modificar el valor de configuración*/
+
+/*muestra el menú principal del administrador y hasta que no pulsa 0 (salir) no termina*/
+void admin_menu(const Config *cfg);
+
+
+#endif
