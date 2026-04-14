427310d3cc02b730047c622671894198409d0ec0
Irene García Aranzabal <irene.g.aranzabal@opendeusto.es>
2026-04-14 20:31:00 +0200

    liberar memoria de configuracion al final del main

--- proyecto/main.c
+++ proyecto/main.c
@@ -43,6 +43,9 @@ int main(void){
     admin_menu(db, &cfg);
     cerrar_baseDatos(db);
 
+    /*liberar memoria de configuración*/
+    config_liberar(&cfg);
+
     printf("\nPrograma finalizado correctamente.\n");
     return 0;
 }
