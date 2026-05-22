d8e74cf88df89443e62832fe6b2241fd22cd073f
maiaalcelaycid <maia.alcelay@opendeusto.es>
2026-05-22 11:28:32 +0200

    servidor la funcion de escuchar

--- proyecto/Socket.cpp
+++ proyecto/Socket.cpp
@@ -10,3 +10,17 @@ Servidor::Servidor() {
     WSAStartup(MAKEWORD(2,0), &WSAData);
     servidor=socket(AF_INET, SOCK_STREAM,0);
 }
+
+bool Servidor::escuchar(int puerto) {
+    servidor = socket(AF_INET, SOCK_STREAM, 0);
+    
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(puerto); 
+    bind(servidor, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+    listen(servidor, 0);
+    
+    cout << "Escuchar en el puerto:  " << puerto << endl;
+    return true;
+}
+     
